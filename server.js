a77ed6e74e61c948ff20a3b18e3cdb75929582f0
Daniel A <dalamo90@gmail.com>
2018-06-12 15:58:35 -0500

    yarn packages installed

--- server.js
+++ server.js
@@ -1,33 +1,32 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 
-var PORT = process.env.PORT || 8080;
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
 
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
+// Initialize Express
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Use body-parser for handling form submissions
+app.use(bodyParser.urlencoded({ extended: true }));
+// Use express.static to serve the public folder as a static directory
 app.use(express.static("public"));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/week18Populater");
 
-// parse application/json
-app.use(bodyParser.json());
-
-// Set Handlebars.
-var exphbs = require("express-handlebars");
-
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Import routes and give the server access to them.
-var routes = require("./controllers/burgers_controller");
-
-app.use(routes);
-
-// Start our server so that it can begin listening to client requests.
-app.listen(PORT, function() {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http://localhost:" + PORT);
-});
-
+// Routes
