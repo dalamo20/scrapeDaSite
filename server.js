55f6d43a2ba93b0d860f0e58935055967a475690
Daniel A <dalamo90@gmail.com>
2018-06-14 00:10:54 -0500

    added correct url and tags to request html on server.js

--- server.js
+++ server.js
@@ -34,21 +34,20 @@ mongoose.connect("mongodb://localhost/week18Populater");
 // A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
   // First, we grab the body of the html with request
-  axios.get("http://www.echojs.com/").then(function(response) {
+  axios.get("https://www.reddit.com/r/dadjokes/").then(function(response) {
     // Then, we load that into cheerio and save it to $ for a shorthand selector
     var $ = cheerio.load(response.data);
 
     // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
+    $("h2.gfYmqG").each(function(i, element) {
       // Save an empty result object
       var result = {};
 
       // Add the text and href of every link, and save them as properties of the result object
       result.title = $(this)
-        .children("a")
         .text();
       result.link = $(this)
-        .children("a")
+        .parent()
         .attr("href");
 
       // Create a new Article using the `result` object built from scraping
